13d6ba57ce1110e72f0405a534f30f2eb97539f0
eherdter <eherdter@mail.usf.edu>
2015-09-16 10:15:45 -0400

    Load all IOA

--- Seatrout IOA.R
+++ Seatrout IOA.R
@@ -1,39 +1,52 @@
 # This is for exploring the indices of abundance and running some statistics on them. 
 setwd("~/Desktop/Github Repo/Seatrout")
-# load Tampa Bay river and bay IOA for YOY. Data are from FIM. 
+# load all IOA for YOY. Data are from FIM. 
 TB_RIV <- read.csv("TB_RIV.csv", check.names=FALSE)
 TB_BAY <- read.csv("TB_BAY.csv", check.names=FALSE)
+AP_BAY <- read.csv("AP_BAY.csv", check.names=FALSE) #there are no IOA for YOY in the river
+CH_BAY <- read.csv("CH_BAY.csv", check.names=FALSE)
+CH_RIV <- read.csv("CH_RIV.csv", check.names=FALSE)
+CK_BAY <- read.csv("CK_BAY.csv", check.names=FALSE)
+CK_RIV <- read.csv("CK_RIV.csv", check.names=FALSE)
+IR_BAY <- read.csv("IR_BAY.csv", check.names=FALSE)
+JX     <- read.csv("JX.csv", check.names=FALSE)
+
+
 
  #melt the data so that the sets can easily be fead into ddply
+library(reshape2)
 meltTB_RIV <- melt(TB_RIV)
-meltTB_BAY<- melt(TB_BAY)
+meltTB_BAY <- melt(TB_BAY)
+meltAP_BAY <- melt(AP_BAY)
+meltCH_BAY <- melt(CH_BAY)
+meltCH_RIV <- melt(CH_RIV)
+meltCK_BAY <- melt(CK_BAY)
+meltCK_RIV <- melt(CK_RIV)
+meltIR_BAY <- melt(IR_BAY)
+meltJX     <- melt(JX)
 
  #re-name column headings
 colnames(meltTB_RIV) <- c("Year", "BootstrapRealization")
-colnames(meltTB_BAY) <- c("Year", "Bootstrap Realization")
+colnames(meltTB_BAY) <- c("Year", "BootstrapRealization")
+colnames(meltAP_BAY) <- c("Year", "BootstrapRealization")
+colnames(meltCH_BAY) <- c("Year", "BootstrapRealization")
+colnames(meltCH_RIV) <- c("Year", "BootstrapRealization")
+colnames(meltCK_BAY) <- c("Year", "BootstrapRealization")
+colnames(meltCK_RIV) <- c("Year", "BootstrapRealization")
+colnames(meltIR_BAY) <- c("Year", "BootstrapRealization")
+colnames(meltJX)     <- c("Year", "BootstrapRealization")
 
 library(plyr)
 TBRIV_sum <- ddply(meltTB_RIV, .(Year), summarise, median=median(BootstrapRealization), lower=quantile(BootstrapRealization, .25, na.rm=TRUE), upper=quantile(BootstrapRealization, .75, na.rm=TRUE))
-
-
-TBBAY_sum <-ddply(meltTB_BAY, .(Year), summarise, median = median(Boostrap Realization), ;)
-
-three <- ddply(data_wkdf_3, .(Year.of.Increment.Formation), summarise, N=length(Increment.Width), three.mean=mean(Increment.Width))
+TBBAY_sum <- ddply(meltTB_BAY, .(Year), summarise, median=median(BootstrapRealization), lower=quantile(BootstrapRealization, .25, na.rm=TRUE), upper=quantile(BootstrapRealization, .75, na.rm=TRUE))
 
 
 
-# calculate median values and quantiles
-library(matrixStats)
-TB_RIV_med <-colMedians(as.matrix(TB_RIV))
-    # set up lines to calculate quantiles
-prob= c(0:4)/4 # defines the percentages that I want
-
-TB_RIV_quan <- lapply(TB_RIV, quantile, probs=prob, name=FALSE)
 
 
-library(plyr)
-TBRIV_sum<- ddply()
 
-TB_BAY_med <- colMedians(as.matrix(TB_BAY))
+
+
+
 
 
