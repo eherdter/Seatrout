e87e5cfecd2068377eae74f953ddf3aae6c1ef5a
eherdter <eherdter@mail.usf.edu>
2015-09-18 16:30:07 -0400

    Script to load raw survey data 9/17/15

--- Seatrout Raw.R
+++ Seatrout Raw.R
@@ -3,5 +3,47 @@
 setwd("~/Desktop/Github Repo/Seatrout/Data/Raw Survey Data/Seatrout FIM Data")
 
 library(haven)
-read_sas("apm_cn_c.sas7bdat")
-read_sas
+#Appalachicola
+AP_C  <- read_sas("apm_cn_c.sas7bdat")
+AP_Hab <- read_sas("apm_cn_hab.sas7bdat")
+AP_Hyd <- read_sas("apm_cn_hyd.sas7bdat")
+AP_L <- read_sas("apm_cn_l.sas7bdat")
+
+#Charlotte Harbor
+CH_C <- read_sas("chm_cn_c.sas7bdat")
+CH_Hab <- read_sas("chm_cn_hab.sas7bdat")
+CH_Hyd <- read_sas("chm_cn_hyd.sas7bdat")
+CH_L <- read_sas("chm_cn_l.sas7bdat")
+
+#Cedar Key
+CK_C <- read_sas("ckm_cn_c.sas7bdat")
+CK_Hab <- read_sas("ckm_cn_hab.sas7bdat")
+CK_Hyd <- read_sas("ckm_cn_hyd.sas7bdat")
+CK_L <- read_sas("ckm_cn_l.sas7bdat")
+
+#Indian River (Northern and Southern Indian River Lagoon???)
+IRM_C <- read_sas("irm_cn_c.sas7bdat")
+IRM_Hab <- read_sas("irm_cn_hab.sas7bdat")
+IRM_Hyd <- read_sas("irm_cn_hyd.sas7bdat")
+IRM_L <- read_sas("irm_cn_l.sas7bdat")
+
+#JAX
+JXM_C <- read_sas("jxm_cn_c.sas7bdat")
+JXM_Hab <- read_sas("jxm_cn_hab.sas7bdat")
+JXM_Hyd <- read_sas("jxm_cn_hyd.sas7bdat")
+JXM_L <- read_sas("jxm_cn_l.sas7bdat")
+
+#Tampa Bay
+TBM_C <- read_sas("tbm_cn_c.sas7bdat")
+TBM_Hab <- read_sas("tbm_cn_hab.sas7bdat")
+TBM_Hyd <- read_sas("tbm_cn_hyd.sas7bdat")
+TBM_L <- read_sas("tbm_cn_l.sas7bdat")
+
+#Tequesta (Southern Indian River Lagoon??)
+TQM_C <- read_sas("tqm_cn_c.sas7bdat")
+TQM_Hab <- read_sas("tqm_cn_hab.sas7bdat")
+TQM_Hyd <- read_sas("tqm_cn_hyd.sas7bdat")
+TQM_L <- read_sas("tqm_cn_l.sas7bdat")
+
+
+
